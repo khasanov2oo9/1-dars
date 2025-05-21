b03653fb0818f9ae1a2f8021e64d6cda73739068
khasanov2oo9 <fazliddinhasanov34@gmail.com>
2025-05-21 18:54:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const ism = prompt("Ismingizni kiriting");
+familiya = prompt("Familiyangizni kiriting");
+let yosh = prompt("Yoshingizni kiriting");
+fan = prompt("Yoqtirgan faningizni kiriting");
+joy = prompt("Yashash tumaningizni kiriting");
+
+console.log(`Ism: ${ism}.`)
+console.log(`Familiya: ${familiya}.`)
+console.log(`Yosh: ${yosh}.`)
+console.log(`Yoqtirgan fan: ${fan}.`)
+console.log(`Yashash manzil: ${joy}.`)
